011ccf14e77a237c7a9b40fc29f50f5ce904d564
vishwaskapte <45562090+vishwaskapte@users.noreply.github.com>
2023-12-23 12:46:54 +0530

    Update package.json file for deployment in netlify site

--- src/Pages/DownloadResume.js
+++ src/Pages/DownloadResume.js
@@ -3,6 +3,6 @@ import resume from './VishwasKapte.pdf'
 
 export default function DownloadResume() {
   return (
-    <a className="social-icon" href= {resume} target="_blank" data-toggle="tooltip" data-placement="top" title="Open Resume in PDF Format" ><i className="fa fa-cloud-download" /></a>
+    <a className="social-icon" href= {resume} rel="noreferrer" target="_blank" data-toggle="tooltip" data-placement="top" title="Open Resume in PDF Format" ><i className="fa fa-cloud-download" /></a>
   )
 }
