d029896900e4fcb81418aaac0fac9a2519ae550d
vishwaskapte <45562090+vishwaskapte@users.noreply.github.com>
2023-12-23 12:40:42 +0530

    remove warnings

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -16,8 +16,8 @@ export default function About() {
             </div>
             <p className="lead mb-5">I am looking for an opportunity to join an organization where I can use my skills and knowledge to contribute as a team member towards the company's growth. I am seeking challenging assignments in the Information Technology industry with a reputable organization that matches my analytical and technical abilities and provides opportunities for personal and professional growth.</p>
             <div className="social-icons">
-              <a className="social-icon" target='_blank' href="https://www.linkedin.com/in/vishwas-kapte-47535621/" data-toggle="tooltip" data-placement="top" title="Open Linekedin Profile"><i className="fab fa-linkedin-in" /></a>
-              <a className="social-icon" target='_blank' href="https://github.com/vishwaskapte" data-toggle="tooltip" data-placement="top" title="Open GitHub Profile"><i className="fab fa-github" /></a>
+              <a className="social-icon" rel="noreferrer" target='_blank' href="https://www.linkedin.com/in/vishwas-kapte-47535621/" data-toggle="tooltip" data-placement="top" title="Open Linekedin Profile"><i className="fab fa-linkedin-in" /></a>
+              <a className="social-icon" rel="noreferrer" target='_blank' href="https://github.com/vishwaskapte" data-toggle="tooltip" data-placement="top" title="Open GitHub Profile"><i className="fab fa-github" /></a>
               <a className="social-icon" href="#!" data-toggle="tooltip" data-placement="top" title="Open Twitter Profile"><i className="fab fa-twitter" /></a>
               <a className="social-icon" href="#!" data-toggle="tooltip" data-placement="top" title="Open Facebook Profile" ><i className="fab fa-facebook-f" /></a>
               <DownloadResume />
